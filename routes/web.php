dcc3ab6050f24b0a9783223e22244167e03195fc
AAKON <faisalakon444@gmail.com>
2021-02-11 20:40:42 +0600

    convert l8

--- routes/web.php
+++ routes/web.php
@@ -29,3 +29,7 @@ Route::get('/icons/svg', 'PagesController@svg');
 
 // Quick search dummy route to display html elements in search dropdown (header search)
 Route::get('/quick-search', 'PagesController@quickSearch')->name('quick-search');
+
+Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
+    return view('dashboard');
+})->name('dashboard');
