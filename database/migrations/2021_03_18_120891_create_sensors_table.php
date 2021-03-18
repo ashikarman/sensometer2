1cf2860f0f896c20d8bc6ee9cc6565db0a191ea0
AAKON <faisalakon444@gmail.com>
2021-03-18 19:22:07 +0600

    base database

---
+++ database/migrations/2021_03_18_120891_create_sensors_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSensorsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sensors', function (Blueprint $table) {
+            $table->id();
+            $table->string('sensor_name')->nullable();
+            $table->integer('slave_low')->nullable();
+            $table->integer('slave_high')->nullable();
+            $table->unsignedBigInteger('device_id')->nullable();
+            $table->foreign('device_id')->references('id')
+            ->on('devices')
+            ->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('sensors');
+    }
+}
