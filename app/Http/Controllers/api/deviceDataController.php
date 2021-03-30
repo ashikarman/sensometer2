b6cff799e64d2b88e875be932d9b6ffebf7b01d3
sakilbd <mdsakiluzzaman@gmail.com>
2021-03-30 14:08:51 +0600

    kushtia code fuckinsadf on progress

--- app/Http/Controllers/api/deviceDataController.php
+++ app/Http/Controllers/api/deviceDataController.php
@@ -10,27 +10,34 @@ class deviceDataController extends Controller
    public function dataTrim(Request $request)
    {
     $data=$request->data;
+#
+#    $config55 = new DeviceData;
+#    $config55->slave_address = $data;
+#    $config55->save();
+#    return 'success';
 
     $array = explode("!", $data);
     $arrayCount = count($array);
     for($f=0;$f<$arrayCount;$f++){
+
+
     $trim = explode("|", $array[$f]);
     $count = count($trim);
-    
-   //  $config = new DeviceData;
+for($x=1;$x<$count;$x++){
+   $config2 = new DeviceData;
     $checkRepeat = DeviceData::where('device_id',$trim[0])->first();
-    
+
    if($count>0){
-   //  $config->device_id = $trim[0];
 
-   for($c=1;$c<$count-1;$c++){
-      
 
-     $sensorData = $trim[$c];
+   for($c=1;$c<2;$c++){
+
+
+     $sensorData = $trim[$x];
      $slave = explode(",", $sensorData);
       $slaveCount = count($slave);
       $slaveAddress = explode("-", $slave[0]);
-      for($e=0;$e<$slaveCount;$e++){
+      for($e=0;$e<=$slaveCount-1;$e++){
          if($e>0){
          $config = new DeviceData;
          $config->device_id = $trim[0];
@@ -45,47 +52,48 @@ class deviceDataController extends Controller
      $config->value = $value[1];
      $config->device_timestamps = $trim[$count-1];
      $config->save();
-     return "Data Inserted";
+    print_r("success");
+
    }
 
    else{
 
-      $config = new DeviceData;
-      $config->device_id = $trim[0];
+      $config1 = new DeviceData;
+      $config1->device_id = $trim[0];
 
       $sensorData1 = $slave[$e];
-      $value = explode("-", $sensorData1);
+      $value1 = explode("-", $sensorData1);
 //   $slave = explode("-", $sensorData);
 
-  $config->slave_address = $value[0];
-  $config->parameter_id = $value[1];
-  $config->value = $value[2];
-  $config->device_timestamps = $trim[$count-1];
-  $config->save();
-return "Data Inserted";
-      
+  $config1->slave_address = $value1[0];
+  $config1->parameter_id = $value1[1];
+  $config1->value = $value1[2];
+  $config1->device_timestamps = $trim[$count-1];
+  $config1->save();
+#print_r("success");
+
    }
+
       }
 
-    
-    
-   // return $slave;
+
+ return "success";
+
    }
 
-    
-   
-
-   //  $config->device_timestamps = $trim[3];
-   
-   
 
 
-   
-   }
 
-   else{
-      return "data Not Inserted";
-   }
+
+
+
+
+
+}
+
+
+
+}
 }
    }
 }
